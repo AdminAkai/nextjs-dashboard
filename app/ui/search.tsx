0693cb2795d4ea056bf641cf00159d3cc4a73837
AdminAkai <akatoirotech@gmail.com>
2025-02-22 20:21:57 -0500

    added debouce

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useDebouncedCallback } from 'use-debounce';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { ChangeEventHandler } from 'react';
 import { usePathname, useSearchParams } from 'next/navigation';
@@ -10,16 +11,17 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const params = new URLSearchParams(searchParams);
+  const handleSearch: ChangeEventHandler<HTMLInputElement> =
+    useDebouncedCallback((e) => {
+      const params = new URLSearchParams(searchParams);
 
-    if (e.target.value) {
-      params.set('query', e.target.value);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  };
+      if (e.target.value) {
+        params.set('query', e.target.value);
+      } else {
+        params.delete('query');
+      }
+      replace(`${pathname}?${params.toString()}`);
+    }, 300);
 
   return (
     <div className='relative flex flex-1 flex-shrink-0'>
@@ -30,6 +32,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className='peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500'
         placeholder={placeholder}
         onChange={handleSearch}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className='absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900' />
     </div>
